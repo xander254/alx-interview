2f44f6ac5626f6c37295b6219725ac369ca50fd9
xander254 <alexkinyanjui356@gmail.com>
2025-07-09 16:07:13 +0300

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Pascal Triangle Func"""
+
+
+def pascal_triangle(n):
+    """Pascal Triangle functtion that returns the triangle"""
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        if i == 0:
+            triangle.append([1]) 
+        else:
+            prev_row = triangle[-1]
+            new_row = [1] 
+
+            for j in range(1, len(prev_row)):
+                new_row.append(prev_row[j - 1] + prev_row[j])
+
+            new_row.append(1)  
+            triangle.append(new_row)
+
+    return triangle
+
