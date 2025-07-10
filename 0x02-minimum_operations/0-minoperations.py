121afa4ddc2c100e79e1169a9a4b8748ea4469b4
xander254 <alexkinyanjui356@gmail.com>
2025-07-11 01:56:16 +0300

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Minimum operations"""
+
+
+def minOperations(n):
+    """Minimum operations"""
+    if n < 2:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n = n // factor
+        factor += 1
+
+    return operations
+    
